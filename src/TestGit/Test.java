640484efba62471d536ea1311f2fd34c1a191675
caopengfei123 <cao20095029@qq.com>
2015-12-03 14:41:41 +0800

    qew

--- src/TestGit/Test.java
+++ src/TestGit/Test.java
@@ -3,6 +3,6 @@ package TestGit;
 public class Test {
 public static void main(String[] args) {
 	System.out.println("这是我的第一个Git项目   ！！！");
-	System.out.println("............");
+	System.out.println("................");
 }
 }
